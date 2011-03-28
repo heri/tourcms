fe8842c4ad1d6d01f6e91874bc0b8153b029662a
Alex Kremer <alex@flextrip.com>
2011-03-28 11:55:20 +0200

    Override Hash just in the TourCMS module

--- lib/tour_cms/hash.rb
+++ lib/tour_cms/hash.rb
@@ -1,8 +1,10 @@
-class Hash
-  def stringify_keys
-    inject({}) do |options, (key, value)|
-      options[key.to_s] = value.to_s
-      options
+module TourCMS
+  class Hash
+    def stringify_keys
+      inject({}) do |options, (key, value)|
+        options[key.to_s] = value.to_s
+        options
+      end
     end
   end
 end
